928e5ba27247b2b94f082a153da8b94386b0390e
Tom <tnguyen171@huskers.unl.edu>
2021-08-27 02:41:13 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,36 @@
+/**
+ * Author: Duy Nguyen
+ *          tnguyen171@huskers.unl.edu
+ * Date: 2021/08/26
+ *
+ * Hack lab #1/**
+ * Author: Duy Nguyen
+ *          tnguyen171@huskers.unl.edu
+ * Date: 2021/08/26
+ *
+ * Hack lab #1
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    printf("Tom Nguyen");
+    return 0;
+
+ }
+
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    printf("Tom Nguyen");
+    return 0;
+
+ }
